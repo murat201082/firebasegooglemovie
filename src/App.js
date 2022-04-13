22371cd27f9b44c5f5f928c66940668bea76e4fb
murat201082 <murat201082@gmail.com>
2022-04-14 00:39:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AuthContextProvider from "./context/AuthContext";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+  return (
+    <div>
+    <AuthContextProvider>
+       <AppRouter />
+    </AuthContextProvider>
+      
+    </div>
+  );
+};
+
+export default App;
